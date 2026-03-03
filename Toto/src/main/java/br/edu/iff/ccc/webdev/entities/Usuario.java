52e2af58df8dbb7391bb7b2d93fa1bcd889e6897
Italo Almeida <gmsitalo@outlook.com>
2026-03-02 21:37:09 -0300

    Adicionando a entidade usuario e seu repositório

---
+++ Toto/src/main/java/br/edu/iff/ccc/webdev/entities/Usuario.java
@@ -0,0 +1,47 @@
+package br.edu.iff.ccc.webdev.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "col_name")
+    private String nome; 
+    private String email;
+
+    public Usuario() {
+    }
+
+    // Getters e Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+}
