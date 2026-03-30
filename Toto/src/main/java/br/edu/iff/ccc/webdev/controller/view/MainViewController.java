469b586be0ca815c2e05395f8e495589386fbb6a
Italo Almeida <gmsitalo@outlook.com>
2026-03-29 22:24:17 -0300

    Configurando o index e o baseLayout

--- Toto/src/main/java/br/edu/iff/ccc/webdev/controller/view/MainViewController.java
+++ Toto/src/main/java/br/edu/iff/ccc/webdev/controller/view/MainViewController.java
@@ -1,19 +1,26 @@
 package br.edu.iff.ccc.webdev.controller.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import br.edu.iff.ccc.webdev.service.DiagramaService;
 
 @Controller
 @RequestMapping("/")
 public class MainViewController {
 
+    private final DiagramaService diagramaService;
+
+    public MainViewController(DiagramaService diagramaService) {
+        this.diagramaService = diagramaService;
+    }
 
     @GetMapping()
-   public String paginaPrincipal() {
-      return "index.html";
-   }
+    public String paginaPrincipal(Model model) {
+        model.addAttribute("diagramas", diagramaService.listarTodos());
+        return "index";
+    }
 
 }
