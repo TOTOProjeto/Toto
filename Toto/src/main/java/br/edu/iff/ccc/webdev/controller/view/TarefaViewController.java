91cfee0ec296e9bb7742f8cdb4bb825c574be7c0
Bruno Wagner <brunowagner087@gmail.com>
2026-03-30 21:32:50 -0300

    Adicionando lista e formulário de tarefas

--- Toto/src/main/java/br/edu/iff/ccc/webdev/controller/view/TarefaViewController.java
+++ Toto/src/main/java/br/edu/iff/ccc/webdev/controller/view/TarefaViewController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.edu.iff.ccc.webdev.dto.TarefaDTO;
 import br.edu.iff.ccc.webdev.entities.Tarefa;
 import br.edu.iff.ccc.webdev.service.TarefaService;
 
@@ -16,6 +19,12 @@ public class TarefaViewController {
 
     @Autowired
     private TarefaService tarefaService;
+
+    @GetMapping
+    public String listarTarefas(Model model) {
+        model.addAttribute("tarefas", tarefaService.listarTodas());
+        return "tarefa-lista";
+    }
 
     @GetMapping("/{id}")
     public String detalhesTarefa(@PathVariable Long id, Model model) {
@@ -28,5 +37,23 @@ public class TarefaViewController {
 
         model.addAttribute("tarefa", tarefa);
         return "tarefa-detalhes";
+    }
+
+    @GetMapping("/new")
+    public String exibirFormulario(Model model) {
+        model.addAttribute("tarefaDTO", new TarefaDTO());
+        return "tarefa-form";
+    }
+
+    @PostMapping("/new")
+    public String salvarTarefa(TarefaDTO tarefaDTO,
+                               RedirectAttributes redirectAttributes) {
+        try {
+            tarefaService.salvar(tarefaDTO);
+            redirectAttributes.addFlashAttribute("successMessage", "Tarefa salva com sucesso!");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Erro ao salvar: " + e.getMessage());
+        }
+        return "redirect:/tarefas";
     }
 }
