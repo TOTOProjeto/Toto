b68193fe66e6b7969b28ccde7c42e32aea7b6c30
Bruno Wagner <brunowagner087@gmail.com>
2026-03-30 21:17:08 -0300

    Exibir em uma template Thymeleaf no mínimo um conteúdo de uma entidade de negócio a partir de uma url específica.

--- Toto/src/main/java/br/edu/iff/ccc/webdev/controller/view/TarefaViewController.java
+++ Toto/src/main/java/br/edu/iff/ccc/webdev/controller/view/TarefaViewController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.edu.iff.ccc.webdev.entities.Tarefa;
 import br.edu.iff.ccc.webdev.service.TarefaService;
 
 @Controller
@@ -15,9 +17,16 @@ public class TarefaViewController {
     @Autowired
     private TarefaService tarefaService;
 
-    @GetMapping
-    public String listarTarefas(Model model) {
-        model.addAttribute("tarefas", tarefaService.listarTodas());
-        return "tarefas"; 
+    @GetMapping("/{id}")
+    public String detalhesTarefa(@PathVariable Long id, Model model) {
+        Tarefa tarefa = tarefaService.buscarPorId(id);
+
+        if (tarefa == null) {
+            model.addAttribute("errorMessage", "Tarefa com ID " + id + " não encontrada.");
+            return "tarefa-detalhes";
+        }
+
+        model.addAttribute("tarefa", tarefa);
+        return "tarefa-detalhes";
     }
 }
